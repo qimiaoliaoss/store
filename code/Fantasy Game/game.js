d8626080fc121be7ad4931884c8b2cc2707b879c
Losir <liao3377853@163.com>
2023-11-23 15:54:18 +0800

    新游戏demo

--- code/Fantasy Game/game.js
+++ code/Fantasy Game/game.js
@@ -95,21 +95,21 @@ let currentMonster = null;
 // 更新游戏状态的函数
 function updateGame() {
   document.getElementById('monster-container').innerHTML = `
-    <p>Monster: ${currentMonster.name}</p>
-    <p>Health: ${currentMonster.health}</p>
-    <p>Mana: ${currentMonster.mana}</p>
-    <p>Defense: ${currentMonster.defense}</p>
-    <p>Damage: ${currentMonster.damage}</p>
-    <p>Skills: ${currentMonster.skills.map(skill => skill.name).join(', ')}</p>
+    <p>怪物: ${currentMonster.name}</p>
+    <p>生命值: ${currentMonster.health}</p>
+    <p>法力值: ${currentMonster.mana}</p>
+    <p>防御力: ${currentMonster.defense}</p>
+    <p>攻击力: ${currentMonster.damage}</p>
+    <p>技能: ${currentMonster.skills.map(skill => skill.name).join(', ')}</p>
   `;
 
   document.getElementById('player-container').innerHTML = `
-    <p>Player Health: ${player.health}</p>
-    <p>Player Mana: ${player.mana}</p>
-    <p>Player Defense: ${player.defense}</p>
-    <p>Player Damage: ${player.damage}</p>
-    <p>Player Skills: ${player.skills.map(skill => skill.name).join(', ')}</p>
-    <p>Inventory: ${player.inventory.join(', ')}</p>
+    <p>玩家生命值: ${player.health}</p>
+    <p>玩家法力值: ${player.mana}</p>
+    <p>玩家防御力: ${player.defense}</p>
+    <p>玩家攻击力: ${player.damage}</p>
+    <p>玩家技能: ${player.skills.map(skill => skill.name).join(', ')}</p>
+    <p>背包: ${player.inventory.join(', ')}</p>
   `;
 }
 
@@ -121,49 +121,49 @@ function encounterMonster() {
   // 更新游戏状态
   updateGame();
 
-  alert(`A wild ${currentMonster.name} appears! Prepare for battle!`);
+  alert(`一只${currentMonster.name}出现了！准备战斗吧！`);
 }
 
 // 攻击函数
 function attack() {
-  // Player uses a random skill
+  // 玩家使用一个随机的技能
   const playerSkill = player.skills[Math.floor(Math.random() * player.skills.length)];
   const playerDamage = playerSkill.effect.call(player);
 
-  // Monster uses a random skill
+  // 怪物使用一个随机的技能
   const monsterSkill = currentMonster.skills[Math.floor(Math.random() * currentMonster.skills.length)];
   const monsterDamage = monsterSkill.effect.call(currentMonster);
 
-  // Apply damage to the monster and player
+  // 对怪物和玩家应用伤害
   currentMonster.health -= playerDamage;
   player.health -= Math.max(0, monsterDamage - player.defense);
 
-  // Update game state
+  // 更新游戏状态
   updateGame();
 
-  // Check if the game is over
+  // 检查游戏是否结束
   checkGameOver();
 }
 
 // 收集战利品函数
 function collectLoot() {
   // 模拟随机掉落的装备物品
-  const lootTypes = ['Sword', 'Potion'];
+  const lootTypes = ['剑', '药水'];
   const randomLootType = lootTypes[Math.floor(Math.random() * lootTypes.length)];
 
   // 根据掉落类型执行相应的操作
   switch (randomLootType) {
-    case 'Sword':
+    case '剑':
       // 获得剑，增加玩家的攻击力
       const swordDamageBonus = 5;
       player.damage += swordDamageBonus;
-      alert(`You found a Sword! Your damage increased by ${swordDamageBonus}.`);
+      alert(`你找到了一把剑！你的攻击力增加了${swordDamageBonus}点。`);
       break;
-    case 'Potion':
+    case '药水':
       // 获得药水，恢复玩家的生命值
       const potionHealAmount = 30;
       player.health = Math.min(100, player.health + potionHealAmount);
-      alert(`You found a Healing Potion! Your health restored by ${potionHealAmount}.`);
+      alert(`你找到了一瓶治疗药水！你的生命值恢复了${potionHealAmount}点。`);
       break;
     // 可以添加更多的战利品类型和效果
   }
@@ -175,14 +175,14 @@ function collectLoot() {
 // 检查游戏是否结束
 function checkGameOver() {
   if (currentMonster.health <= 0) {
-    alert(`You defeated the ${currentMonster.name}! Victory!`);
+    alert(`你战胜了${currentMonster.name}！胜利！`);
 
     // 移动收集战利品的逻辑到战胜怪物后
     collectLoot();
 
     encounterMonster();
   } else if (player.health <= 0) {
-    alert('Game over! The monster defeated you.');
+    alert('游戏结束！怪物战胜了你。');
     resetGame();
   }
 }
@@ -195,16 +195,16 @@ function resetGame() {
   player.damage = 15;
   player.skills = [
     {
-      name: 'Attack',
+      name: '攻击',
       effect: function () {
-        return this.damage; // Basic attack
+        return this.damage; // 基础攻击
       },
     },
     {
-      name: 'Heal',
+      name: '治疗',
       effect: function () {
         const healAmount = 20;
-        this.health = Math.min(100, this.health + healAmount); // Heal for 20 health points
+        this.health = Math.min(100, this.health + healAmount); // 治疗 20 点生命值
         return healAmount;
       },
     },
@@ -213,7 +213,7 @@ function resetGame() {
 
   currentMonster = null;
 
-  alert('Game reset. Start a new adventure!');
+  alert('游戏重置。开始新的冒险！');
   encounterMonster();
 }
 
